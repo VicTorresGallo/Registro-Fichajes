93404b93360f883b6e65f2efc5d2c2c571adc1e2
VicTorresGallo <vicentg1211@gmail.com>
2025-10-16 10:09:50 +0200

    blank

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Fichajes',
+  webDir: 'www'
+};
+
+export default config;
